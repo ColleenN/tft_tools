2407e626eeabe18ebc1c8616c346aed30ddf72ba
CNelson <cnelson@multimediallc.com>
2024-11-15 20:43:28 -0700

    Code for assembling graph data structure for unit-based nodes

---
+++ graph/unit.py
@@ -0,0 +1,30 @@
+from data_io.tft import get_set_json, get_base_data, get_unique_traits, COLOR_MAP
+import networkx as nx
+import itertools
+
+
+def get_unit_node_graph(set_num, costs=None):
+
+    if not costs:
+        costs = list(range(1, 6))
+
+    graph = nx.Graph()
+    for champ in get_base_data(set_num):
+        if champ['cost'] not in costs:
+            continue
+        graph.add_node(
+            champ['name'],
+            cost=champ['cost'],
+            label=champ['name'],
+            color=COLOR_MAP[champ['cost']],
+            traits=champ['traits']
+        )
+
+    trait_map = nx.get_node_attributes(graph, 'traits')
+    for node_1, node_2 in itertools.combinations(graph.nodes, 2):
+        shared_traits = set(trait_map[node_1]) & set(trait_map[node_2])
+        if shared_traits:
+            trait_string = ', '.join(shared_traits)
+            graph.add_edge(node_1, node_2, label=trait_string)
+
+    return graph
