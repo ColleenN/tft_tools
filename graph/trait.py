75889f5495409e7ee4671fd748803baed483bda8
CNelson <colleenfnelson@gmail.com>
2024-11-26 21:49:22 -0700

    Fix for change in unique trait fcn

--- graph/trait.py
+++ graph/trait.py
@@ -1,11 +1,11 @@
 import networkx as nx
 
-from data_io.tft import get_base_data, get_unique_traits, COLOR_MAP
+from data_io.tft import get_base_unit_data, get_unique_traits, COLOR_MAP
 
 
 def get_trait_node_graph(set_num, show_unique_traits=False, costs=list(range(1, 6))):
     edge_list = []
-    for champ in get_base_data(set_num):
+    for champ in get_base_unit_data(set_num):
         if champ['cost'] not in costs:
             continue
         edge_attrs = {
@@ -22,7 +22,8 @@ def get_trait_node_graph(set_num, show_unique_traits=False, costs=list(range(1, 
     graph = nx.MultiGraph(edge_list)
 
     if not show_unique_traits:
-        graph.remove_nodes_from(get_unique_traits(set_num))
+        trait_names = [x[0] for x in get_unique_traits(set_num)]
+        graph.remove_nodes_from(trait_names)
 
     return graph
 
