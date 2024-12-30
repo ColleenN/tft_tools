cfde3e974ce968ce79831e2bd83a0ec059949e18
CNelson <cnelson@multimediallc.com>
2024-12-29 20:23:30 -0700

    Retry & timeout settings

--- data_io/gcp.py
+++ data_io/gcp.py
@@ -3,12 +3,12 @@ from google.cloud import storage
 
 def read_from_bucket(bucket_name, file_path):
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
+    bucket = storage_client.get_bucket(bucket_name, timeout=.1, retry=None)
     return bucket.blob(file_path).download_as_string()
 
 
 def write_to_bucket(bucket_name, file_path, data, mode):
     storage_client = storage.Client()
-    bucket = storage_client.get_bucket(bucket_name)
+    bucket = storage_client.get_bucket(bucket_name, timeout=.1, retry=None)
     with bucket.blob(file_path).open(mode, encoding='utf-8') as f:
         f.write(data)
