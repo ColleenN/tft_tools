bc6448d3c8b5e7a948a93517227b2143f850ffef
CNelson <colleenfnelson@gmail.com>
2025-03-27 01:56:08 -0600

    Include item detail + default to latest set when set num not given

--- data_io/tft.py
+++ data_io/tft.py
@@ -5,10 +5,19 @@ from pathlib import Path
 
 class TFTSetBlob:
 
-    def __init__(self, set_num):
+    def __init__(self, set_num = None):
         self._set_num = set_num
         self.offline = False
         self._data = {}
+        self._item_data = []
+        self._base = {}
+
+    @property
+    def mutator(self):
+        if self._set_num == 13:
+            return 'TFTSet13_Evolved_MacaoMode'
+        else:
+            return f'TFTSet{self._set_num}'
 
     def _load_data(self):
         if not self.offline:
@@ -17,18 +26,36 @@ class TFTSetBlob:
         else:
             with Path('../json_samples/en_us.json').open() as f:
                 base = json.loads(f.read())
+
+        self._base = base
+
+        if not self._set_num:
+            self._set_num = max([x['number'] for x in base['setData']])
+
+
         for item in base['setData']:
-            if item['mutator'] == f'TFTSet{self._set_num}':
+            if item['mutator'] == self.mutator:
                 self._data = item
+                self._load_set_data_items(base['items'], item)
                 return
 
-        raise ValueError('Could not located requested set json')
+        raise ValueError('Could not locate requested set json')
+
+    def _load_set_data_items(self, data_item_listing, set_data):
+        include = set_data['items'] + set_data['augments']
+        self._item_data = [x for x in data_item_listing if x['apiName'] in include]
 
     @property
     def data(self):
         if not self._data:
             self._load_data()
         return self._data
+
+    @property
+    def data_item_detail(self):
+        if not self._item_data:
+            self._load_data()
+        return self._item_data
 
     def get_emblem_list(self):
         return [i_name for i_name in self.data['items'] if 'Emblem' in i_name]
